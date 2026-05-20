5165246640bdf7215bdd761f4a6139a016baaaea
Nico Caminero <nerdboy0428@gmail.com>
2026-05-20 17:41:49 -0400

    feat: add skills section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import SelectedWork from "./components/SelectedWork";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <SelectedWork />
+      <Skills />
     </>
   );
 }
